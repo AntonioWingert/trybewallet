f567797a43d2f610c3e09a71f9cfaa997dd8c3a2
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-12-08 14:52:39 -0300

    iniciando o projeto

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,8 @@
-// configure aqui sua store
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
